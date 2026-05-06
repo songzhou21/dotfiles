b45a1a5223a48a274c866d64e6810da8dec9b342
songzhou <songzhou21@outlook.com>
2026-05-06 14:43:21 +0800

    update sourcekit lsp config to support C-family languages

--- .config/nvim/lsp/sourcekit.lua
+++ .config/nvim/lsp/sourcekit.lua
@@ -1,7 +1,7 @@
 -- lsp/sourcekit.lua
 return {
   cmd = { 'xcrun', 'sourcekit-lsp' },
-  filetypes = { 'swift' },
+  filetypes = { 'swift', 'objective-c', 'objective-cpp', 'c', 'cpp' },
   root_markers = {
     'buildServer.json',
     'compile_commands.json',
@@ -10,6 +10,8 @@ return {
     '.git',
   },
   get_language_id = function(_, ftype)
+    if ftype == 'objective-c' then return 'objective-c' end
+    if ftype == 'objective-cpp' then return 'objective-cpp' end
     return ftype
   end,
   capabilities = {
